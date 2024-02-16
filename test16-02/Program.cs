cfdf5b9accb44c79fd8cd503e1fa16783c8f3bb6
Martinphurrr <martinmarincev22@gmail.com>
2024-02-16 10:41:14 +0200

    made a year checking console app

--- test16-02/Program.cs
+++ test16-02/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Martin!");
+Console.WriteLine("Въведете годините ви:");
+ int age = int.Parse(Console.ReadLine());
+if (age < 18)
+{
+    Console.WriteLine("непълнолетен");
+}
+else
+{
+    Console.WriteLine("пълнолетен");
+}
