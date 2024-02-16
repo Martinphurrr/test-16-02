5fa35b0ecf6d36348f2d3ca9b9799a8dcabe874b
Martinphurrr <martinmarincev22@gmail.com>
2024-02-16 10:44:30 +0200

    add 16

--- test16-02/Program.cs
+++ test16-02/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Въведете годините ви:");
  int age = int.Parse(Console.ReadLine());
-if (age < 18)
+if (age < 16)
 {
     Console.WriteLine("непълнолетен");
 }
